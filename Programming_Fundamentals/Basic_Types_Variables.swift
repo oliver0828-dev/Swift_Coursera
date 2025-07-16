910089c7c9ebc34c06c6bcdb0c29c05813203b6b
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2025-07-16 20:43:59 +0900

    Update Basic_Types_Variables.swift

--- Programming_Fundamentals/Basic_Types_Variables.swift
+++ Programming_Fundamentals/Basic_Types_Variables.swift
@@ -24,3 +24,23 @@ let levelLowestScore = 50
 let levelHighestScore = 99
 let levels = 10
 
+// Strings
+let day = "Monday"
+print("Today is \(day)")
+
+let hour = "6"
+let minute = "15"
+let currentPeriod = "PM"
+
+var time = hour + ":" + minute + " " + currentPeriod
+print("It is \(time).")
+
+let timeZone = "PST"
+
+time += " \(timeZone)"
+print("It is \(time).")
+
+let shortDay = day.prefix(3)
+time += ", \(shortDay)"
+
+print("It is \(time).")
