98d13a233873aec4c13a5303060a942bfb76ce04
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2025-07-16 20:34:40 +0900

    Update Basic_Types_Variables.swift

--- Programming_Fundamentals/Basic_Types_Variables.swift
+++ Programming_Fundamentals/Basic_Types_Variables.swift
@@ -4,3 +4,23 @@ let dailyTemperature = "75"
 
 print("Today is \(day). Rise and Shine!")
 print("\(day)'s Temperature is \(dailyTemperature)°F")
+
+// Operators
+let levelScore = 10
+var gameScore = 0
+gameScore += levelScore
+
+print("The game score is \(gameScore)")
+
+var levelBonusScore = 10.0
+
+print("The level bonus score is \(levelBonusScore)")
+
+gameScore += Int(levelBonusScore)
+
+print("Game Final Score is \(gameScore)")
+
+let levelLowestScore = 50
+let levelHighestScore = 99
+let levels = 10
+
