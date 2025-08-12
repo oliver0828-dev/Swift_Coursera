15c1574055b7c117590756334a7d5c24cc9d6b40
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2025-08-12 23:07:56 +0900

    Update Functions.swift

--- Programming_Fundamentals/Functions.swift
+++ Programming_Fundamentals/Functions.swift
@@ -27,3 +27,13 @@ incrementInventory(&goldBars)
 print(goldBars)
 
 
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
